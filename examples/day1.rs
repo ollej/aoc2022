3083bbfdb09ad91a6d7fbe9ca57f1ecef0210d35
Olle Wreede <olle@wreede.se>
2022-12-02 11:48:31 +0100

    Revert "Change to only use a single value for the maximum"
    This reverts commit e59d4c9d8523d07124bb03c68ccc1e037cb17197.

--- examples/day1.rs
+++ examples/day1.rs
@@ -1,17 +1,19 @@
 use std::{error::Error, fs};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut max_calories = 0;
+    let mut gnomes = vec![];
     let mut calories = 0;
     let input = fs::read_to_string("examples/input1.txt")?;
     for line in input.lines() {
         if line.is_empty() {
-            max_calories = max_calories.max(calories);
+            gnomes.push(calories);
             calories = 0;
         } else {
             calories += line.parse::<i32>()?;
         }
     }
-    println!("Gnome with most calories: {}", max_calories);
+    gnomes.sort();
+    gnomes.reverse();
+    println!("Gnome with most calories: {}", gnomes.first().unwrap());
     Ok(())
 }
